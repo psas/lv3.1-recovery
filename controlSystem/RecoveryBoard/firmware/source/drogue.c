26cc39d8b47a01451d898fa64c4f5c16f1300b5f
erfi-x2 <hessalsa@pdx.edu>
2025-09-25 15:10:09 -0700

    Enable interrupt events and loop threads

--- controlSystem/RecoveryBoard/firmware/source/drogue.c
+++ controlSystem/RecoveryBoard/firmware/source/drogue.c
@@ -1,6 +1,6 @@
 #include "ch.h"
-#include "chprintf.h"
 #include "hal.h"
+#include "chprintf.h"
 #include "recovery.h"
 
 THD_WORKING_AREA(waDrogueThread, 256);
@@ -8,16 +8,21 @@ THD_WORKING_AREA(waDrogueThread, 256);
 THD_FUNCTION(DrogueThread, arg) {
   (void)arg;
   chRegSetThreadName("Drogue");
-  msg_t drogue_line = palWaitLineTimeout(LINE_ISO_DROGUE, TIME_INFINITE);
+  palEnableLineEvent(LINE_ISO_DROGUE, PAL_EVENT_MODE_RISING_EDGE);
+  while (true) {
+    chThdSleepMilliseconds(10);  // Wait for printout (100 char ~ 10 ms)
+    chprintf(DEBUG_SD, "drogue line is alive\r\n");
+    msg_t drogue_line = palWaitLineTimeout(LINE_ISO_DROGUE, TIME_INFINITE);
 
-  if (drogue_line == MSG_OK) {
-    chprintf(DEBUG_SD, "Drogue Signal: High");
-  }
-  // don't care for these, just want to check if any signal is returned
-  if (drogue_line == MSG_RESET) {
-    chprintf(DEBUG_SD, "Drogue Signal: Reset");
-  }
-  if (drogue_line == MSG_TIMEOUT) {
-    chprintf(DEBUG_SD, "Drogue Signal: TIMEOUT");
+    if (drogue_line == MSG_OK) {
+      chprintf(DEBUG_SD, "Drogue Signal: High\r\n");
+    }
+    // don't care for these, just want to check if any signal is returned
+    if (drogue_line == MSG_RESET) {
+      chprintf(DEBUG_SD, "Drogue Signal: Reset\r\n");
+    }
+    if (drogue_line == MSG_TIMEOUT) {
+      chprintf(DEBUG_SD, "Drogue Signal: TIMEOUT\r\n");
+    }
   }
 }
--- controlSystem/RecoveryBoard/firmware/source/mainchute.c
+++ controlSystem/RecoveryBoard/firmware/source/mainchute.c
@@ -7,9 +7,16 @@ THD_WORKING_AREA(waMainchuteThread, 256);
 
 THD_FUNCTION(MainchuteThread, arg) {
   (void)arg;
+
   chRegSetThreadName("Mainchute");
-  msg_t main_line = palWaitLineTimeout(LINE_ISO_MAIN, TIME_INFINITE);
-  if (main_line == MSG_OK) {
-    chprintf(DEBUG_SD, "Main Signal: High");
+  palEnableLineEvent(LINE_ISO_MAIN, PAL_EVENT_MODE_RISING_EDGE);
+  while (true) {
+    chprintf(DEBUG_SD, "main line is alive\r\n");
+
+    msg_t main_line = palWaitLineTimeout(LINE_ISO_MAIN, TIME_INFINITE);
+
+    if (main_line == MSG_OK) {
+      chprintf(DEBUG_SD, "Main Signal: High\r\n");
+    }
   }
 }
