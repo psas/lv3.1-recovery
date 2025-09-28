356a6104ccab5bb664eaa548ef132c88e22c40f6
Will <wjeffreys96@gmail.com>
2025-09-27 17:02:47 -0700

    WIP: hall sensor and motor drive

--- controlSystem/RecoveryBoard/firmware-rs/src/shared/adc.rs
+++ controlSystem/RecoveryBoard/firmware-rs/src/shared/adc.rs
@@ -10,10 +10,7 @@ use embassy_time::Timer;
 pub static BATT_READ_CHANNEL: Channel<CriticalSectionRawMutex, u8, 10> = Channel::new();
 
 #[embassy_executor::task]
-pub async fn read_battery(
-    mut adc: Adc<'static, ADC1>,
-    mut pb0: Peri<'static, PB0>,
-) {
+pub async fn read_battery(mut adc: Adc<'static, ADC1>, mut pb0: Peri<'static, PB0>) {
     loop {
         let adc_read = adc.read(&mut pb0).await;
         let v_batt = ((adc_read as f32) / 4096.0 * 3.3) / 0.2326;
@@ -24,10 +21,7 @@ pub async fn read_battery(
 }
 
 #[embassy_executor::task]
-pub async fn read_battery_from_ref(
-    adc: &'static AdcType,
-    mut pb0: Peri<'static, PB0>,
-) {
+pub async fn read_battery_from_ref(adc: &'static AdcType, mut pb0: Peri<'static, PB0>) {
     loop {
         {
             let mut adc_unlocked = adc.lock().await;
@@ -36,7 +30,6 @@ pub async fn read_battery_from_ref(
                 let v_batt = ((adc_read as f32) / 4096.0 * 3.3) / 0.2326;
                 let batt_sig = (v_batt / 0.1) as u8;
                 BATT_READ_CHANNEL.send(batt_sig).await;
-
             }
         }
         Timer::after_secs(1).await;
