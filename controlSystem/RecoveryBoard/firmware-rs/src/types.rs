663a98eefeba309f7b0c911298e92f8515515142
William <will@localhost.localdomain>
2025-11-01 15:01:14 -0700

    Delete unused types

--- controlSystem/RecoveryBoard/firmware-rs/src/types.rs
+++ controlSystem/RecoveryBoard/firmware-rs/src/types.rs
@@ -10,7 +10,6 @@ use embassy_stm32::{
 use embassy_sync::{
     blocking_mutex::raw::{CriticalSectionRawMutex, ThreadModeRawMutex},
     mutex::Mutex,
-    signal::Signal,
 };
 
 pub type PwmType = Mutex<ThreadModeRawMutex, Option<SimplePwm<'static, TIM15>>>;
@@ -18,5 +17,3 @@ pub type UmbOnType = Mutex<ThreadModeRawMutex, Option<Input<'static>>>;
 pub type CanType = Mutex<ThreadModeRawMutex, Option<Can<'static>>>;
 pub type AdcType = Mutex<CriticalSectionRawMutex, Option<Adc<'static, ADC1>>>;
 pub type DacType = Mutex<ThreadModeRawMutex, Option<Dac<'static, DAC1, Async>>>;
-pub type BattOkSignalType = Signal<CriticalSectionRawMutex, u8>;
-pub type ShortPowOnSignalType = Signal<CriticalSectionRawMutex, bool>;
