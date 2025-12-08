bcb86183ac8f89fd97bd0f63f3a8ac4fb227257c
Theodore Havelka <tedhavelka66@gmail.com>
2025-12-07 21:49:53 -0800

    app: DTS and pwm.c modified, buzzer now working but LED no longer showing . . .
    updates to PWM period.

--- controlSystem/RecoveryBoard/firmware-zephyr/apps/src/pwm.c
+++ controlSystem/RecoveryBoard/firmware-zephyr/apps/src/pwm.c
@@ -11,17 +11,36 @@
 
 LOG_MODULE_REGISTER(pwm, LOG_LEVEL_INF);
 
-static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_buzzer));
-// static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
+static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
+static const struct pwm_dt_spec pwm_buzzer = PWM_DT_SPEC_GET(DT_ALIAS(buzzer));
 
 #define MIN_PERIOD PWM_SEC(1U) / 128U
 #define MAX_PERIOD PWM_SEC(1U)
 
 static uint32_t max_period_fs;
 
-int32_t pwm_init(void)
+//----------------------------------------------------------------------
+// - SECTION - routines
+//----------------------------------------------------------------------
+
+int32_t pwm_init_alt_channel(void)
 {
 	LOG_INF("PWM set up for ERS buzzer\n");
+
+	if (!pwm_is_ready_dt(&pwm_buzzer)) {
+		LOG_ERR("Error: PWM device %s is not ready\n",
+		       pwm_buzzer.dev->name);
+		return -ENODEV;
+	}
+
+	return 0;
+}
+
+int32_t pwm_init(void)
+{
+	int32_t rc = 0;
+
+	LOG_INF("initializing PWM for device '%s'\n", pwm_led0.dev->name);
 
 	if (!pwm_is_ready_dt(&pwm_led0)) {
 		LOG_ERR("Error: PWM device %s is not ready\n",
@@ -51,41 +70,59 @@ int32_t pwm_init(void)
 	LOG_INF("Done calibrating; maximum/minimum periods %u/%lu nsec\n",
 	       max_period_fs, MIN_PERIOD);
 
-	return 0;
+	rc = pwm_init_alt_channel();
+	return rc;
 }
 
 /*
 Using period 15625000
-Using period 7812500
-Using period 15625000
-Using period 31250000
-Using period 62500000
-Using period 125000000
-Using period 250000000
-Using period 500000000
+Using period 7812500   . . . 256
+Using period 15625000  . . . 128
+Using period 31250000   . . . 64
+Using period 62500000   . . . 32
+Using period 125000000  . . . 16
+Using period 250000000   . . . 8
+Using period 500000000   . . . 4
 Using period 1000000000  . . . 2 Hertz
 Using period 500000000
+
+From ChibiOS ERS work, source file beep.c:
+
+ 16   // Sweeps through the speaker frequency range once a second from low to high
+ 17   // by changing the PWM period. At a PWM timer rate of 100khz the periods
+ 18   // correspond to:
+ 19   // - 28 -> ~3570Hz
+ 20   // - 25 ->  4000Hz
+ 21   // - 23 -> ~4350Hz
 */
 
 int32_t pwm_play_melody(void)
 {
 	uint32_t period;
-	// uint8_t dir = 0U;
 	uint32_t note_count = 5;
 	int ret;
 
 // Use 500000000 to achieve 2Hz PWD signal on ERS orange LED
 // (buzzer will require much higher frequency to produce audible sound)
-#define DEV_STARTING_PERIOD 500000000 // 7812500 // 62500000
+#define DEV_STARTING_PERIOD 62500000
 #define DEV_MAX_PERIOD 1000000000
 	// period = max_period_fs;
 	period = DEV_STARTING_PERIOD;
 	while (note_count > 0) {
 		ret = pwm_set_dt(&pwm_led0, period, period / 2U);
 		if (ret) {
-			LOG_ERR("Error %d: failed to set pulse width\n", ret);
+			LOG_ERR("Error %d: failed to set pulse width", ret);
+			LOG_ERR("on device %s", pwm_led0.dev->name);
 			return 0;
 		}
+#if 1
+		ret = pwm_set_dt(&pwm_buzzer, period, period / 2U);
+		if (ret) {
+			LOG_ERR("Error %d: failed to set pulse width", ret);
+			LOG_ERR("on device %s", pwm_buzzer.dev->name);
+			return 0;
+		}
+#endif // 0
 		LOG_INF("Using period %d\n", period);
 
 		period = (period / 2U);
