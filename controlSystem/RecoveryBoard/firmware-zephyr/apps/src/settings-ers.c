0d41ef0dfc350b33f484e1c397157384f297f592
Theodore Havelka <tedhavelka66@gmail.com>
2026-01-04 09:32:27 -0800

    app: settings module: remove a bunch of sample code not applicable to ERS.

--- controlSystem/RecoveryBoard/firmware-zephyr/apps/src/settings-ers.c
+++ controlSystem/RecoveryBoard/firmware-zephyr/apps/src/settings-ers.c
@@ -25,31 +25,7 @@ LOG_MODULE_REGISTER(ers_settings, CONFIG_ERS_SETTINGS_LOG_LEVEL);
 // - SECTION - file scoped
 //----------------------------------------------------------------------
 
-// --- ZEPHYR 3.7.0 SETTINGS SAMPLE CODE BEGIN ---
-/* Default values are assigned to settings values consuments
- * All of them will be overwritten if storage contain proper key-values
- */
-uint8_t angle_val;
-uint64_t length_val = 100;
-uint16_t length_1_val = 40; 
-uint32_t length_2_val = 60; 
-int32_t voltage_val = -3000;
-char source_name_val[6] = "";
-
-int alpha_handle_set(const char *name, size_t len, settings_read_cb read_cb,
-		     void *cb_arg);
-int alpha_handle_commit(void);
-int alpha_handle_export(int (*cb)(const char *name,
-			const void *value, size_t val_len));
-
-/* dynamic main tree handler */
-struct settings_handler alph_handler = {
-                .name = "alpha",
-                .h_get = NULL,
-                .h_set = alpha_handle_set,
-                .h_commit = alpha_handle_commit,
-                .h_export = alpha_handle_export
-};
+// Note following structure borrowed from Zephyr 3.7.0 settings sample code:
 
 struct direct_immediate_value {
 	size_t len;
@@ -57,80 +33,9 @@ struct direct_immediate_value {
 	uint8_t fetched;
 };
 
-// --- ZEPHYR 3.7.0 SETTINGS SAMPLE CODE END ---
-
 //----------------------------------------------------------------------
 // - SECTION - routines
 //----------------------------------------------------------------------
-
-int alpha_handle_set(const char *name, size_t len, settings_read_cb read_cb,
-		  void *cb_arg)
-{
-	const char *next;
-	size_t next_len;
-	int rc;
-
-	if (settings_name_steq(name, "angle/1", &next) && !next) {
-		if (len != sizeof(angle_val)) {
-			return -EINVAL;
-		}
-		rc = read_cb(cb_arg, &angle_val, sizeof(angle_val));
-		LOG_INF("<alpha/angle/1> = %d", angle_val);
-		return 0;
-	}
-
-	next_len = settings_name_next(name, &next);
-
-	if (!next) {
-		return -ENOENT;
-	}
-
-	if (!strncmp(name, "length", next_len)) {
-		next_len = settings_name_next(name, &next);
-
-		if (!next) {
-			rc = read_cb(cb_arg, &length_val, sizeof(length_val));
-			LOG_INF("<alpha/length> = %" PRId64, length_val);
-			return 0;
-		}
-
-		if (!strncmp(next, "1", next_len)) {
-			rc = read_cb(cb_arg, &length_1_val,
-				     sizeof(length_1_val));
-			LOG_INF("<alpha/length/1> = %d", length_1_val);
-			return 0;
-		}
-
-		if (!strncmp(next, "2", next_len)) {
-			rc = read_cb(cb_arg, &length_2_val,
-				     sizeof(length_2_val));
-			LOG_INF("<alpha/length/2> = %d", length_2_val);
-			return 0;
-		}
-
-		return -ENOENT;
-	}
-
-	return -ENOENT;
-}
-
-int alpha_handle_commit(void)
-{
-	LOG_INF("loading all settings under <alpha> handler is done");
-	return 0;
-}
-
-int alpha_handle_export(int (*cb)(const char *name,
-			       const void *value, size_t val_len))
-{
-	LOG_INF("export keys under <alpha> handler");
-	(void)cb("alpha/angle/1", &angle_val, sizeof(angle_val));
-	(void)cb("alpha/length", &length_val, sizeof(length_val));
-	(void)cb("alpha/length/1", &length_1_val, sizeof(length_1_val));
-	(void)cb("alpha/length/2", &length_2_val, sizeof(length_2_val));
-
-	return 0;
-}
 
 static int direct_loader_immediate_value(const char *name, size_t len,
 					 settings_read_cb read_cb, void *cb_arg,
@@ -231,49 +136,4 @@ void ers_settings_init(void)
 	}
 
 	LOG_INF("settings subsystem initialization OK.");
-
-	rc = settings_register(&alph_handler);
-	if (rc) {
-		LOG_ERR("Failed to register subtree <%s> handler, err %d",
-		       alph_handler.name, rc);
-	}
-
-	LOG_INF("subtree <%s> handler registered: OK", alph_handler.name);
-	LOG_INF("subtree <alpha/beta> has static handler");
-
-// TODO [ ] - BEGIN - remove following local proof of concept code
-#if 0
-#define DEV_MOTOR_USE_COUNT_STARTING_VALUE 5
-	uint32_t val = 0;
-	uint32_t i;
-	for (i = 0; i < 3; i++) {
-
-		LOG_INF("*****  settings exercise iteration %u:  ******", i);
-		rc = retrieve_ers_setting("motor_uses", &val, sizeof(val));
-		LOG_INF("- DEV 1130 - call to retrieve motor use count returns status %d", rc);
-		if (val > ((1 << 10) - 1))
-		{
-			val = 0;
-		}
-
-		if (rc != 0)
-		{
-			val = DEV_MOTOR_USE_COUNT_STARTING_VALUE;
-		}
-		else
-		{
-			LOG_INF("- DEV 1130 - read motor use count of %u", val);
-			k_msleep(10);
-
-			val += 2;
-			LOG_INF("- DEV 1130 - incrementing value to %u", val);
-			k_msleep(10);
-		}
-	
-		rc = store_ers_setting("motor_uses", (const void *)val, sizeof(val));
-		LOG_INF("- DEV 1130 - call to store motor use count returns status %d", rc);
-		k_msleep(2000);
-	}
-#endif // 0
-// TODO [ ] - END - remove following local proof of concept code
 }
