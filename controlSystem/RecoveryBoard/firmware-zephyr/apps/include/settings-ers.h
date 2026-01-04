3e6089869c61488925bfd5e03cdb5f8e94a55eda
Theodore Havelka <tedhavelka66@gmail.com>
2026-01-04 09:20:53 -0800

    app: update both keeper and motor modules to correctly store parachute ring . . .
    lock event and unlock event counts.

--- controlSystem/RecoveryBoard/firmware-zephyr/apps/include/settings-ers.h
+++ controlSystem/RecoveryBoard/firmware-zephyr/apps/include/settings-ers.h
@@ -1,6 +1,11 @@
 #ifndef SETTINGS_ERS_H
 #define SETTINGS_ERS_H
 
+#define KEY_NAME_LOCK_COUNT "lock_count"
+#define KEY_NAME_UNLOCK_COUNT "unlock_count"
+#define RING_LOCK_EVENT_STARTING_COUNT 0
+#define RING_UNLOCK_EVENT_STARTING_COUNT 0
+
 void ers_settings_init(void);
 
 int32_t retrieve_ers_setting(const char* name, void *val, const uint32_t size);
