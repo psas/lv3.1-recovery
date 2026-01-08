4ecc9f2dcdac0d6f516bf3c2b724b3c5273d5297
Theodore Havelka <tedhavelka66@gmail.com>
2026-01-07 22:46:04 -0800

    scripts: amend the Python helper script which listens for CAN message to . . .
    periodically send a mocked Telemetrum heartbeat message.  This supports testing
    of ERS drogue firmware logic which watches for the heartbeat and adjusts its
    summary flash values to reflect when Telemetrum is detected at at least one half
    Hertz.

--- controlSystem/RecoveryBoard/firmware-zephyr/scripts/rx.py
+++ controlSystem/RecoveryBoard/firmware-zephyr/scripts/rx.py
@@ -15,13 +15,34 @@ bus = can.interface.Bus(channel=channel, interface="slcan", bitrate=bitrate)
 timeout = 86400 # as found value 10
 end_time = time.time() + timeout
 
+def send_frame(iter, id):
+    message_id = id
+    data_bytes = [0x00, 0x00]
+    message = can.Message(arbitration_id=message_id, data=data_bytes, is_extended_id=True)
+    bus.send(message)
+    print(f"Sent message: {message}")
+
 print(f"Listening for CAN messages for {timeout} seconds...")
+
+i = 0
+toggle = 0
 
 # Receive CAN messages until timeout is reached
 while time.time() < end_time:
     message = bus.recv(timeout=end_time - time.time())
     if message:
         print(f"Received message: {message}")
+
+    i = i + 1
+    # print(f"- MARK - {i}")
+    if i % 10 == 0:
+        if toggle == 0:
+            toggle = 1
+        else:
+            toggle = 0
+    if toggle == 1:
+        send_frame(i, 0x700)
+    time.sleep(1/1)
 
 # Close the bus connection
 bus.shutdown()
