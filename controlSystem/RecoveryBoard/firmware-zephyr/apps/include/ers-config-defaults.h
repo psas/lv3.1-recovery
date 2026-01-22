d8d0e6af5eed414469de09b8428b029e77ba43d3
Theodore Havelka <tedhavelka66@gmail.com>
2026-01-22 00:16:38 -0800

    app: significant renaming of Hall sensor attributes, limits and states . . .
    and start of larger clean up in arbiter and keeper modules.  Spent good amount
    of time working with "x macros" in attempt to better organize and factor the
    numerous symbols and variable names associated with Hall sensor limits.  Macros
    may be removed but for this commit they provide a somewhat more central place
    to edit the emperically discovered Hall sensor limit values, measured in ADC
    counts.  Plus some other symbol name choice now grouped more closely to on
    another.

--- controlSystem/RecoveryBoard/firmware-zephyr/apps/include/ers-config-defaults.h
+++ controlSystem/RecoveryBoard/firmware-zephyr/apps/include/ers-config-defaults.h
@@ -23,77 +23,95 @@ Unactive      |   locked    LOCKED   between     error     locked
 Under         |   error     locked   between   unlocked     error
 */
 
-// - X MACRO 1 - Hall sensor default empirical limits per sensor
-
-// Define an outer macro which encapsulates empirically determined Hall sensor limit values:
+/**
+ * (C1) Hall sensor default limit values
+ */
 
 #define HALL_AND_SENSORS_DEFAULT_LIMIT_VALUES \
 HALL_SENSOR_INST(1, 750, 920, 1600, 3100) \
 HALL_SENSOR_INST(2, 800, 950, 2600, 3100)
 
-// Create identifiers for physical Hall sensors attached to ERS board:
+/**
+ * (C2) Hall sensor instance enumeration
+ */
 
 #define HALL_SENSOR_INST(sensor_idx, limit1, limit2, limit3, limit4) HALL_SENSOR_##sensor_idx,
-
 enum hall_sensor_instances {
 HALL_AND_SENSORS_DEFAULT_LIMIT_VALUES
 HALL_SENSOR_COUNT
 };
-
 #undef HALL_SENSOR_INST
 
-// - X MACRO 2 -
+/**
+ * (C3) Hall sensor state names
+ */
 
-// (1) limit identifier
-// (2) per sensor limit identifier
-// (3) limit value
-// (4) settings keyname for limit value
-// (5) Hall output state
+#define  LIMIT_NAME_1  HALL_LIMIT_V_UNDER
+#define  LIMIT_NAME_2  HALL_LIMIT_V_INACTIVE
+#define  LIMIT_NAME_3  HALL_LIMIT_V_BETWEEN
+#define  LIMIT_NAME_4  HALL_LIMIT_V_ACTIVE
 
-// clang-format off
-#define HALL_SENSOR_INST(idx, limit1, limit2, limit3, limit4) \
-HALL_SENSOR_CONSTRUCT(HL_V_UNDER,   HALL_##idx##_LIMIT_V_UNDER, limit1, "hall-##idx##-limit-v-under",  HALL_OUTPUT_UNDER_VOLTAGE) \
-HALL_SENSOR_CONSTRUCT(HL_INACTIVE, HALL_##idx##_LIMIT_INACTIVE, limit2, "hall-##idx##-limit-inactive", HALL_OUTPUT_INACTIVE)      \
-HALL_SENSOR_CONSTRUCT(HL_BETWEEN,   HALL_##idx##_LIMIT_BETWEEN, limit3, "hall-##idx##-limit-between",  HALL_OUTPUT_BETWEEN)       \
-HALL_SENSOR_CONSTRUCT(HL_ACTIVE,     HALL_##idx##_LIMIT_ACTIVE, limit4, "hall-##idx##-limit-active",   HALL_OUTPUT_ACTIVE)        \
-//                    ^^^^^^^^^^^   ^^^^^^^^^^^^^^^^^^^^^^^^^^  ^^^^^   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^  ^^^^^^^^^^^^^^^^^^^^^^^^^
-//                     |             |                           |       |                             |
-//                    limit ids     per sensor limit ids        limit   settings keyname              Hall state identifiers
-//                                                              values
-// clang-format on
+#define  LIMIT_VAR_1  hall_limit_v_under
+#define  LIMIT_VAR_2  hall_limit_v_inactive
+#define  LIMIT_VAR_3  hall_limit_v_between
+#define  LIMIT_VAR_4  hall_limit_v_active
 
-// Create identifiers for Hall sensor limits:
+#define  STATE_NAME_1  HALL_STATE_V_UNDER
+#define  STATE_NAME_2  HALL_STATE_V_INACTIVE
+#define  STATE_NAME_3  HALL_STATE_V_BETWEEN
+#define  STATE_NAME_4  HALL_STATE_V_ACTIVE
+#define  STATE_NAME_5  HALL_STATE_V_OVER
+#define  STATE_NAME_6  HALL_STATE_UNKNOWN
 
-#define HALL_SENSOR_CONSTRUCT(limit, per_sensor_limit, value, keyname, hall_state) limit,
+/**
+ * (C4) Enumeration of Hall sensor limits (named identifiers):
+ */
 
-enum hall_sensor_limit_ids {
-HALL_SENSOR_INST(0, 0, 0, 0, 0)
+enum hall_sensor_named_limits {
+LIMIT_NAME_1,
+LIMIT_NAME_2,
+LIMIT_NAME_3,
+LIMIT_NAME_4,
 HALL_SENSOR_LIMIT_COUNT
 };
 
-#undef HALL_SENSOR_CONSTRUCT
+#define CONCAT_A_B_C(prefix, infix, name) prefix ## infix ## name
+#define INTERMEDIATE_A_B_C(prefix, infix, name) CONCAT_A_B_C(prefix, infix, name)
 
-// Create symbols for Hall sensor default limit values:
-// (Values determined empirically from lock ring assembly measurement exercises.)
+#define LIMIT_NAME(prefix, infix, name) CONCAT_A_B_C(prefix, infix, name)
 
-#define HALL_SENSOR_CONSTRUCT(limit, per_sensor_limit, value, keyname, hall_state) per_sensor_limit = value,
+#define HALL_SENSOR_INST(idx, limit1, limit2, limit3, limit4) \
+HALL_SENSOR_CONSTRUCT(LIMIT_NAME(LIMIT_NAME_1, _S, idx), LIMIT_NAME(LIMIT_VAR_1, _S, idx), limit1) \
+HALL_SENSOR_CONSTRUCT(LIMIT_NAME(LIMIT_NAME_2, _S, idx), LIMIT_NAME(LIMIT_VAR_2, _S, idx), limit2) \
+HALL_SENSOR_CONSTRUCT(LIMIT_NAME(LIMIT_NAME_3, _S, idx), LIMIT_NAME(LIMIT_VAR_3, _S, idx), limit3) \
+HALL_SENSOR_CONSTRUCT(LIMIT_NAME(LIMIT_NAME_4, _S, idx), LIMIT_NAME(LIMIT_VAR_4, _S, idx), limit4)
 
+/**
+ * (C5) Hall sensor limit variable names
+ */
+
+#define HALL_SENSOR_CONSTRUCT(limit_name, limit_var_name, limit_value) \
+	limit_name = limit_value,
 enum hall_sensor_limit_defaults {
-HALL_AND_SENSORS_DEFAULT_LIMIT_VALUES
+	HALL_AND_SENSORS_DEFAULT_LIMIT_VALUES
 };
-
 #undef HALL_SENSOR_CONSTRUCT
 
-// Create Hall sensor state ids:
-
-#define HALL_SENSOR_CONSTRUCT(limit, per_sensor_limit, value, keyname, hall_state) hall_state,
+/**
+ * (C6) Hall sensor named states
+ */
 
 enum hall_sensor_state_ids {
-HALL_SENSOR_INST(0, 0, 0, 0, 0)
-HALL_OUTPUT_OVER_VOLTAGE,
-HALL_OUTPUT_UNKNOWN
+STATE_NAME_1,
+STATE_NAME_2,
+STATE_NAME_3,
+STATE_NAME_4,
+STATE_NAME_5,
+STATE_NAME_6,
 };
 
 #undef HALL_SENSOR_CONSTRUCT
+
+#define BASE_10 10
 
 #endif // ERS_CONFIG_DEFAULTS
