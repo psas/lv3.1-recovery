ef99b91d437fda4169e55e3f9f0165c3ec6a79dd
Theodore Havelka <tedhavelka66@gmail.com>
2026-08-22 07:16:43 +0000

    app: amend PWM module with a thread of its own, so that audio tones and
    sequences of tones may be directed by other modules, while the details of
    producing those tone patterns are encapsulated in the PWM module.

--- controlSystem/RecoveryBoard/firmware-zephyr/apps/include/ers-pwm.h
+++ controlSystem/RecoveryBoard/firmware-zephyr/apps/include/ers-pwm.h
@@ -1,8 +1,8 @@
 #ifndef ERS_PWM_H
 #define ERS_PWM_H
 
-int pwm_init(void);
-
 int32_t pwm_play_melody(void);
+
+int32_t pwm_init(void);
 
 #endif // ERS_PWM_H
--- controlSystem/RecoveryBoard/firmware-zephyr/apps/src/pwm.c
+++ controlSystem/RecoveryBoard/firmware-zephyr/apps/src/pwm.c
@@ -18,15 +18,21 @@ static const struct pwm_dt_spec pwm_buzzer = PWM_DT_SPEC_GET(DT_ALIAS(buzzer));
 
 static uint32_t max_period_fs;
 
+struct k_thread pwm_thread_data;
+
+K_THREAD_STACK_DEFINE(pwm_thread_stack, CONFIG_PWM_THREAD_STACK_SIZE);
+
+#define PWM_MAX_HEARTBEAT_COUNT 5
+
 //----------------------------------------------------------------------
 // - SECTION - routines
 //----------------------------------------------------------------------
 
-int32_t pwm_init(void)
+static int32_t init_pwm_hardware(void)
 {
 	int32_t rc = 0;
 
-	LOG_INF("initializing PWM for device '%s'\n", pwm_buzzer.dev->name);
+	// LOG_INF("initializing PWM for device '%s'\n", pwm_buzzer.dev->name);
 
 	if (!pwm_is_ready_dt(&pwm_buzzer)) {
 		LOG_ERR("Error: PWM device %s is not ready\n",
@@ -41,7 +47,7 @@ int32_t pwm_init(void)
 	 * Keep its value at least MIN_PERIOD * 4 to make sure
 	 * the sample changes frequency at least once.
 	 */
-	LOG_INF("Calibrating for channel %d...\n", pwm_buzzer.channel);
+	// LOG_INF("Calibrating for channel %d...\n", pwm_buzzer.channel);
 	max_period_fs = MAX_PERIOD;
 	while (pwm_set_dt(&pwm_buzzer, max_period_fs, max_period_fs / 2U)) {
 		max_period_fs /= 2U;
@@ -85,8 +91,7 @@ static int32_t buzzer_heartbeat(void)
 {
 	static uint32_t call_count = 0;
 
-	if (call_count > 3)
-	{
+	if (call_count > PWM_MAX_HEARTBEAT_COUNT) {
 		return 0;
 	}
 	call_count++;
@@ -109,10 +114,53 @@ static int32_t buzzer_heartbeat(void)
 int32_t pwm_play_melody(void)
 {
 	int rc = buzzer_heartbeat();
-	if (rc != 0)
-	{
+	if (rc != 0) {
 		LOG_ERR("Failed to produce buzzer heartbeat audio, err %d", rc);
 	}
-	k_sleep(K_SECONDS(2U));
+	// k_sleep(K_SECONDS(2U));
 	return 0;
+}
+
+void pwm_thread_entry(void *arg1, void *arg2, void *arg3)
+{
+	ARG_UNUSED(arg1);
+	ARG_UNUSED(arg2);
+	ARG_UNUSED(arg3);
+
+	int32_t rc = 0;
+
+	while (1) {
+		rc = pwm_play_melody();
+		if (rc < 0) {
+			LOG_ERR("Failed to play current audio note, err %d", rc);
+		}
+		// TODO [ ] Create symbol for PWM thread sleep period
+		k_msleep(750);
+	}
+}
+
+int32_t pwm_init(void)
+{
+	int32_t rc = init_pwm_hardware();
+	if (rc < 0) {
+		LOG_ERR("Failed to initialize PWM peripheral, err %d", rc);
+		goto done;
+	}
+
+	k_tid_t pwm_tid = k_thread_create(&pwm_thread_data,
+					pwm_thread_stack,
+					K_THREAD_STACK_SIZEOF(pwm_thread_stack),
+					pwm_thread_entry,
+					NULL, NULL, NULL,
+					CONFIG_PWM_THREAD_PRIORITY,
+					0, K_NO_WAIT);
+
+	k_thread_name_set(pwm_tid, "pwm_thread");
+
+	if (!pwm_tid) {
+		LOG_ERR("ERROR spawning PWM thread\n");
+	}
+
+done:
+	return rc;
 }
