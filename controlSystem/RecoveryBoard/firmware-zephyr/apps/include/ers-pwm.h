56ffd2fedd45ace12eb71e3f5c987c5b1700da6b
Theodore Havelka <tedhavelka66@gmail.com>
2026-08-23 05:40:36 +0000

    app: PWM module: add state machine and audio pattern struct to support flexible
    audio patterns.  Add a simple public API to the new audio patterns supporting
    code.

--- controlSystem/RecoveryBoard/firmware-zephyr/apps/include/ers-pwm.h
+++ controlSystem/RecoveryBoard/firmware-zephyr/apps/include/ers-pwm.h
@@ -1,7 +1,20 @@
 #ifndef ERS_PWM_H
 #define ERS_PWM_H
 
-int32_t pwm_play_melody(void);
+#include <stdint.h>
+
+enum pwm_audio_pattern {
+	PWM_APP_SILENT,
+	PWM_APP_BANNER_PATTERN,
+	PWM_APP_HEARTBEAT,
+	PWM_ROCKET_READY,
+};
+
+/**
+ * @brief Function to take a caller's requested pattern and start that playing.
+ */
+
+int32_t pwm_play_pattern(enum pwm_audio_pattern);
 
 int32_t pwm_init(void);
 
