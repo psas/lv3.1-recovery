a16bc9f305af69e022a9faca08de0e0353b45b14
Theodore Havelka <tedhavelka66@gmail.com>
2026-09-01 06:28:29 +0000

    app: add mutex protection to ADC public APIs.

--- controlSystem/RecoveryBoard/firmware-zephyr/apps/src/adc.c
+++ controlSystem/RecoveryBoard/firmware-zephyr/apps/src/adc.c
@@ -67,10 +67,9 @@ static const struct adc_dt_spec adc_channels[] = {
                  DT_SPEC_AND_COMMA)
 };
 
-// TODO [ ] Compare thread configuration code in can.c with what is here:
-struct k_thread adc_thread_data;
-
 K_THREAD_STACK_DEFINE(adc_thread_stack, CONFIG_ADC_THREAD_STACK_SIZE);
+
+struct k_thread adc_thread_data;
 
 struct k_mutex adc_mtx;
 
@@ -78,26 +77,13 @@ struct k_mutex adc_mtx;
 // - SECTION - routines
 //----------------------------------------------------------------------
 
-// TODO [ ] Amend the "read ADC all" command to accept a range of channels,
-//   to support the reading of one channel with the same routine.
-
-// TODO [x] Implement logic to update both Hall sensor values when "read ADC
-//   channels" API is called to read all channels.  (This to assue that Hall
-//   sensor readings are from the same cycle of readings, and not the past
-//   two cycles.)
-
-// TODO [ ] Check that following API to read ADC channel range has complete
-//   comment block in ers-adc.h header file.
-
 int32_t adc_read_channels(const enum ers_adc_values idx_begin,
 			  const enum ers_adc_values idx_end)
 {
-
-        int32_t rc = k_mutex_lock(&adc_mtx, K_MSEC(1500));
-	if (rc != 0)
-	{
+	int32_t rc = k_mutex_lock(&adc_mtx, K_MSEC(1500));
+	if (rc != 0) {
 		LOG_ERR("Failed to lock ADC read channels mutex, error %d", rc);
-		return rc;
+		goto done;
 	}
 
 	if ((idx_begin < 0) || (idx_end > ARRAY_SIZE(adc_channels)))
@@ -105,7 +91,7 @@ int32_t adc_read_channels(const enum ers_adc_values idx_begin,
 		LOG_ERR("Asked to read ADC channel out of range!");
 		LOG_ERR("One of begin %d and end %d out of range %d..%d",
 			idx_begin, idx_end, 0, ARRAY_SIZE(adc_channels));
-		return -EINVAL;
+		goto unlock;
 	}
 
         uint16_t buf;
@@ -115,8 +101,7 @@ int32_t adc_read_channels(const enum ers_adc_values idx_begin,
                 .buffer_size = sizeof(buf),
         };
 
-        for (size_t i = idx_begin; i <= idx_end; i++)
-        {
+        for (size_t i = idx_begin; i <= idx_end; i++) {
                 int32_t val_mv;
 
 #ifdef DEV_ERS_ADC_PERIODIC_REPORTING
@@ -128,8 +113,7 @@ int32_t adc_read_channels(const enum ers_adc_values idx_begin,
                 (void)adc_sequence_init_dt(&adc_channels[i], &sequence);
 
                 rc = adc_read_dt(&adc_channels[i], &sequence);
-                if (rc < 0)
-                {
+                if (rc < 0) {
                         LOG_ERR("Could not read ADC channel, error (%d)", rc);
                         continue;
                 }
@@ -142,12 +126,9 @@ int32_t adc_read_channels(const enum ers_adc_values idx_begin,
                  * in the ADC sample buffer should be a signed 2's
                  * complement value.
                  */
-                if (adc_channels[i].channel_cfg.differential)
-                {
+                if (adc_channels[i].channel_cfg.differential) {
                         val_mv = (int32_t)((int16_t)buf);
-                }
-                else
-                {
+                } else {
                         val_mv = (int32_t)buf;
                 }
 
@@ -156,12 +137,9 @@ int32_t adc_read_channels(const enum ers_adc_values idx_begin,
 #endif
                 rc = adc_raw_to_millivolts_dt(&adc_channels[i], &val_mv);
                 /* conversion to mV may not be supported, skip if not */
-                if (rc < 0)
-                {
+                if (rc < 0) {
                         LOG_WRN(" (value in mV not available)");
-                }
-                else
-                {
+                } else {
 			// Store ADC reading in ERS app "keeper" module:
 			keeper_set_adc_value_in_mv(i, (uint32_t)buf);
 #ifdef DEV_ERS_ADC_PERIODIC_REPORTING
@@ -170,51 +148,70 @@ int32_t adc_read_channels(const enum ers_adc_values idx_begin,
 		}
 	}
 
-        rc = k_mutex_unlock(&adc_mtx);
-	if (rc != 0)
-	{
-		LOG_ERR("Failed to unlock ADC read channels mutex, error %d", rc);
+unlock:
+	rc = k_mutex_unlock(&adc_mtx);
+	if (rc != 0) {
+		LOG_ERR("Failed to unlock mutex in ADC read channels, err %d", rc);
 		return rc;
 	}
 
+done:
 	return rc;
 }
 
 int32_t cmd_ers_read_adc_in0(const struct shell *shell)
 {
 	uint32_t adc_reading;
-	int32_t rc;
+
+	int32_t rc = k_mutex_lock(&adc_mtx, K_MSEC(1500));
+	if (rc != 0) {
+		LOG_ERR("Failed to lock mutex in read ADC_IN0, err %d", rc);
+		goto done;
+	}
 
 	rc = adc_read_channels(ADC_READING_HALL_1, ADC_READING_HALL_1);
-	if (rc == 0)
-	{
+	if (rc == 0) {
 		keeper_get_hall_1_mv(&adc_reading);
 		shell_fprintf(shell, SHELL_NORMAL, "Hall 1 sensor reads %u mV\n", adc_reading);
-	}
-	else
-	{
+	} else {
 		shell_fprintf(shell, SHELL_NORMAL, "failed to read Hall 1 sensor, err %d\n", rc);
 	}
 
+	rc = k_mutex_unlock(&adc_mtx);
+	if (rc != 0) {
+		LOG_ERR("Failed to unlock mutex in read ADC_IN0, err %d", rc);
+		return rc;
+	}
+
+done:
 	return rc;
 }
 
 int32_t cmd_ers_read_adc_in1(const struct shell *shell)
 {
 	uint32_t adc_reading;
-	int32_t rc;
+
+	int32_t rc = k_mutex_lock(&adc_mtx, K_MSEC(1500));
+	if (rc != 0) {
+		LOG_ERR("Failed to lock mutex in read ADC_IN1, err %d", rc);
+		goto done;
+	}
 
 	rc = adc_read_channels(ADC_READING_HALL_2, ADC_READING_HALL_2);
-	if (rc == 0)
-	{
+	if (rc == 0) {
 		keeper_get_hall_2_mv(&adc_reading);
 		shell_fprintf(shell, SHELL_NORMAL, "Hall 2 sensor reads %u mV\n", adc_reading);
-	}
-	else
-	{
+	} else {
 		shell_fprintf(shell, SHELL_NORMAL, "failed to read Hall 2 sensor, err %d\n", rc);
 	}
 
+	rc = k_mutex_unlock(&adc_mtx);
+	if (rc != 0) {
+		LOG_ERR("Failed to unlock mutex in read ADC_IN1, err %d", rc);
+		return rc;
+	}
+
+done:
 	return rc;
 }
 
@@ -239,12 +236,14 @@ void adc_thread_entry(void *arg1, void *arg2, void *arg3)
                 if (!adc_is_ready_dt(&adc_channels[i])) {
                         LOG_ERR("ADC controller device %s not ready\n", adc_channels[i].dev->name);
                         rc = -ENODEV;
+			goto error;
                 }
 
                 rc = adc_channel_setup_dt(&adc_channels[i]);
                 if (rc < 0) {
                         LOG_ERR("Could not setup channel #%d (%d)\n", i, rc);
                         rc = -EINVAL;
+			goto error;
                 }
         }
 
@@ -259,6 +258,9 @@ void adc_thread_entry(void *arg1, void *arg2, void *arg3)
 		rc = adc_read_channels(ADC_READING_HALL_1, ADC_READING_MOTOR_ISENSE); // adc_thread_entry()
                 k_sleep(K_MSEC(ADC_READ_PERIOD_MS));
         }
+
+error:
+	return;
 }
 
 int32_t adc_init(void)
@@ -289,15 +291,13 @@ int32_t adc_init(void)
 
 	k_thread_name_set(adc_tid, "adc_thread");
 
-	if (!adc_tid)
-	{
+	if (!adc_tid) {
 		LOG_ERR("ERROR spawning ADC thread\n");
-	}
-	else
-	{
+	} else {
 		LOG_INF("starting ADC thread . . .");
 	}
 
 	LOG_INF("- DEV 1026 - ADC module configured %u channels.", ARRAY_SIZE(adc_channels));
+
 	return rc;
 }
